360c37005e05c291c9c4e30901762230fd0ba13e
samugd17 <samugd17@gmail.com>
2023-12-16 12:41:51 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// https://laravel.com/api/6.x/Illuminate/Support/Facades/DB.html
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+  $wished = DB::select('select * from places where visited = false');
+  $visited = DB::select('select * from places where visited = true');
+
+  return view('travelroad', ['wished' => $wished, 'visited' => $visited]);
 });
